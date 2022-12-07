075d24fc54ebeea12442e86ddef73786d421ff71
qiqi.jin <qiqi.jin@alibaba-inc.com>
2022-12-07 23:46:34 +0800

    init

---
+++ ws-chat-gpt.el
@@ -0,0 +1,89 @@
+;;; ws-chat-gpt.el --- Plugin ChatGPT for websocket bridge.  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Qiqi Jin
+
+;; Author: Qiqi Jin <ginqi7@gmail.com>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+(require 'websocket-bridge)
+
+(defgroup
+  ws-chat-gpt()
+  "Check grammar in buffers by chat-gpt."
+  :group 'applications)
+
+(defcustom ws-chat-gpt-debug nil
+  "If open debug mode.
+default is nil.
+t will open debug mode, the python code will show playwright window.")
+
+(defvar ws-chat-gpt-py-path
+  (concat (file-name-directory load-file-name) "ws_chat_gpt.py"))
+
+
+(defun ws-chat-gpt-start ()
+  "Start websocket bridge chat-gpt."
+  (interactive)
+  (websocket-bridge-app-start "chat-gpt" "python3" ws-chat-gpt-py-path))
+
+
+(defun ws-chat-gpt-restart ()
+  "Restart websocket bridge chat-gpt and show process."
+  (interactive)
+  (websocket-bridge-app-exit "chat-gpt")
+  (ws-chat-gpt-start)
+  (websocket-bridge-app-open-buffer "chat-gpt"))
+
+(defun ws-chat-gpt-input ()
+  "Send input to chatGPT."
+  (interactive)
+  (ws-chat-gpt-send-request "input"
+                            (read-string
+                             (format "Tell chatGPT: (%s): "
+                                     (thing-at-point 'line))
+                             nil
+                             nil
+                             (thing-at-point 'line))))
+
+(defun ws-chat-gpt-send-request (func-name &optional parameter)
+  "Send request to chatGPT, with FUNC-NAME PARAMETER."
+  (websocket-bridge-call "chat-gpt" func-name parameter))
+
+(defun ws-chat-gpt-refresh()
+  "Refresh ChatGPT page."
+  (interactive)
+  (ws-chat-gpt-send-request "refresh"))
+
+(defun ws-chat-gpt-render (html)
+  "Called by python, to render HTML.
+HTML is the chatGPT resutl."
+  (pop-to-buffer "*chatGPT*")
+  (setq buffer-read-only nil)
+  (erase-buffer)
+  (shr-insert-document
+   (with-temp-buffer
+     (insert html)
+     (libxml-parse-html-region (point-min) (point-max))))
+  (goto-char (point-min))
+  (special-mode))
+
+(provide 'ws-chat-gpt)
+;;; ws-chat-gpt.el ends here
---
+++ ws_chat_gpt.py
@@ -0,0 +1,83 @@
+'''Websocket Bridge extension for chatGPT'''
+import asyncio
+import json
+
+import websocket_bridge_python
+from playwright.async_api import async_playwright
+from pycookiecheat import chrome_cookies
+
+conversation_count = 0
+
+async def connect_chat_gpt(playwright):
+    """using playwright to connect chatGPT."""
+    global PAGE, CONTEXT, BRIDGE
+    browser_type = playwright.chromium
+    debug_mode = await BRIDGE.get_emacs_var("ws-chat-gpt-debug")
+    debug_mode = bool(debug_mode == 'true');
+    browser = await browser_type.launch(headless=(not debug_mode))
+    CONTEXT = await browser.new_context()
+    cookie_array = []
+    cookies = chrome_cookies("https://chat.openai.com")
+    cookie_array = [
+        {"name": x, "value": y, "path": "/", "domain": "chat.openai.com", "secure": True}
+        for x, y in cookies.items()
+    ]
+    await CONTEXT.add_cookies(cookie_array)
+    PAGE = await CONTEXT.new_page()
+    await PAGE.goto("https://chat.openai.com/chat")
+    f12 = await PAGE.context.new_cdp_session(PAGE)
+    await f12.send("Network.enable")
+    await f12.send("Page.enable")
+    f12.on("Network.responseReceived", handle_websocket_frame_received)
+
+async def handle_websocket_frame_received(params:dict):
+    """define a handler for chrome received websockets."""
+    global conversation_count
+    if params["response"]:
+        if params["response"]["url"] == 'https://chat.openai.com/backend-api/conversation':
+            await fetch_anwser(conversation_count)
+            conversation_count = conversation_count + 1
+
+async def fetch_anwser(number):
+    '''fetch chatGPT anwser'''
+    class_name = f'.request-\\:R2d6\\:-{number}'
+    anwser = await PAGE.wait_for_selector(class_name)
+    class_name = f'.request-\\\\:R2d6\\\\:-{number}'
+    js = f'() => document.querySelector("{class_name}").parentNode.nextSibling'
+    await PAGE.wait_for_function(js)
+    html = (await anwser.inner_html()).replace('"', "'")
+    await BRIDGE.eval_in_emacs(f'(ws-chat-gpt-render "{html}")')
+
+async def input_text(text):
+    '''input text in chatGPT textarea'''
+    await PAGE.type("textarea", f"{text}\n")
+
+async def refresh():
+    '''refresh chatGPT page'''
+    global conversation_count
+    await PAGE.reload()
+    conversation_count = 0
+
+async def on_message(message):
+    """dispatch message recived from Emacs."""
+    info = json.loads(message)
+    cmd = info[1][0].strip()
+    sentence_str = info[1][1]
+    if cmd == "input":
+        await input_text(sentence_str)
+    elif cmd == "refresh":
+        await refresh()
+    else:
+        print("not fount dispatcher", flush=True)
+
+async def main():
+    """main"""
+    global BRIDGE
+    async with async_playwright() as playwright:
+        try:
+            BRIDGE = websocket_bridge_python.bridge_app_regist(on_message)
+            await asyncio.gather(connect_chat_gpt(playwright), BRIDGE.start())
+        except TimeoutError:
+            print("Timeout!")
+
+asyncio.run(main())
